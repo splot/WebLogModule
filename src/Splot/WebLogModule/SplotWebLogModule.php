3d71025d7dd17ac1edafebe8719b19ec8519a2ed
Michał Dudek <michal@michaldudek.pl>
2014-03-02 15:47:38 +0100

    Working with MD\Clog instead of Splot\Log.

--- src/Splot/WebLogModule/SplotWebLogModule.php
+++ src/Splot/WebLogModule/SplotWebLogModule.php
@@ -2,10 +2,8 @@
 namespace Splot\WebLogModule;
 
 use MD\Foundation\Debug\Debugger;
+use MD\Foundation\Utils\ArrayUtils;
 use MD\Foundation\Utils\StringUtils;
-
-use Splot\Log\LogContainer;
-use Splot\Log\ExportableLogInterface;
 
 use Splot\Framework\Modules\AbstractModule;
 use Splot\Framework\Events\WillSendResponse;
@@ -14,34 +12,31 @@ class SplotWebLogModule extends AbstractModule
 {
 
     public function boot() {
-        if (LogContainer::isEnabled()) {
-            $container = $this->container;
+        $container = $this->container;
 
-            $container->get('event_manager')->subscribe(WillSendResponse::getName(), function(WillSendResponse $event) use ($container) {
-                $benchmark = array();
+        $container->get('event_manager')->subscribe(WillSendResponse::getName(), function(WillSendResponse $event) use ($container) {
+            $benchmark = array();
 
-                $logsContent = Debugger::consoleStringDump('######################### SPLOT LOG #########################');
-                foreach(LogContainer::getLogs() as $name => $log) {
-                    if (!$log instanceof ExportableLogInterface) {
-                        continue;
-                    }
+            $logsContent = Debugger::consoleStringDump('######################### SPLOT LOG #########################');
+            $messages = $container->get('clog.writer.memory')->getMessages();
+            $messages = ArrayUtils::groupBy($messages, '_name');
 
-                    $logsContent .= Debugger::consoleStringDump('####### '. $name);
-                    foreach($log->getLog() as $item) {
-                        $logsContent .= Debugger::consoleStringDump($item);
-                    }
+            foreach($messages as $name => $msgs) {
+                $logsContent .= Debugger::consoleStringDump('####### '. $name);
+                foreach($msgs as $item) {
+                    $logsContent .= Debugger::consoleStringDump($item);
                 }
+            }
 
-                // log benchmark data
-                $timer = $container->get('application')->getTimer();
-                $logsContent .= Debugger::consoleStringDump('####### Benchmark', array(
-                    'Execution Time' => round($timer->stop() * 1000) . ' ms',
-                    'Memory Used' => StringUtils::bytesToString($timer->getStopMemoryPeak())
-                ));
+            // log benchmark data
+            $timer = $container->get('application')->getTimer();
+            $logsContent .= Debugger::consoleStringDump('####### Benchmark', array(
+                'Execution Time' => round($timer->stop() * 1000) . ' ms',
+                'Memory Used' => StringUtils::bytesToString($timer->getStopMemoryPeak())
+            ));
 
-                $event->getResponse()->alterPart('</body>', '<script type="text/javascript">'. $logsContent .'</script>'. NL .'</body>');
-            }, -99999);
-        }
+            $event->getResponse()->alterPart('</body>', '<script type="text/javascript">'. $logsContent .'</script>'. NL .'</body>');
+        }, -99999);
     }
 
 }
